1d1f827218576fdfb1fc0adb9fcd9ff6984aa7a6
irgigi <79409202+irgigi@users.noreply.github.com>
2024-01-29 17:36:26 +0500

    Initial Commit

---
+++ PetsBookApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PetsBookApp
+//
+//  Created by Мамуля on 29.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
